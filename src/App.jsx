94c9355a488f90e0f7e9e4428a20eae61470aa85
Minh <tranhoangminh0410@gmail.com>
2025-02-07 10:21:04 +0700

    Navbar 7/2/2.25

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,35 @@ import "./App.css";
 import Picture from "./components/Picture";
 import museumLogo from "./assets/museum-logo.png";
 
+function changeFont(event) {
+  var id = event.target.id;
+  // Tìm tất cả các phần tử có class "navButton button"
+  const navButtons = document.querySelectorAll(".navButton button");
+  const navTextButtons = document.querySelectorAll(".navTextButton .Text");
+  if (id === "0") {
+    navButtons.forEach((button) => {
+      button.style.fontSize = "1.35em";
+    });
+    navTextButtons.forEach((buttonFont) => {
+      buttonFont.style.fontSize = "1.3em";
+    });
+  } else if (id === "1") {
+    navButtons.forEach((button) => {
+      button.style.fontSize = "1.15em";
+    });
+    navTextButtons.forEach((buttonFont) => {
+      buttonFont.style.fontSize = "1.1em";
+    });
+  } else if (id === "2") {
+    navButtons.forEach((button) => {
+      button.style.fontSize = "0.95em";
+    });
+    navTextButtons.forEach((buttonFont) => {
+      buttonFont.style.fontSize = "0.9em";
+    });
+  }
+}
+
 function App() {
   return (
     <nav
@@ -13,26 +42,42 @@ function App() {
         <div className="col-span-1 ">
           <Picture className="object-cover" url={museumLogo} alt="none" />
         </div>
-        <div className="col-span-4 grid grid-rows-3 gap-2">
+        <div className="col-span-4 grid grid-rows-2 gap-2">
           <div className="grid grid-cols-4">
             <div className="col-start-4 flex items-center navTextButton">
               <div className="Text mr-4">Front Size:</div>
-              <button className="bg-neutral-950 rounded-sm text-white mr-2">
+              <button
+                id="0"
+                style={{ cursor: "pointer" }}
+                onClick={changeFont}
+                className="bg-neutral-950 rounded-sm text-white mr-2"
+              >
                 L
               </button>
-              <button className="bg-neutral-950 rounded-sm text-white mr-2">
+              <button
+                id="1"
+                onClick={changeFont}
+                style={{ cursor: "pointer" }}
+                className="bg-neutral-950 rounded-sm text-white mr-2"
+              >
                 M
               </button>
-              <button className="bg-neutral-950 rounded-sm text-white">
+              <button
+                id="2"
+                onClick={changeFont}
+                style={{ cursor: "pointer" }}
+                className="bg-neutral-950 rounded-sm text-white"
+              >
                 S
               </button>
             </div>
           </div>
-          <div className="row-span-2 items-end justify-items-center grid grid-cols-4 navButton">
+          <div className="flex justify-around items-end  navButton">
             <button className="">Collections</button>
             <button className="">Tour Guide</button>
-            <button className="">Visit ticket</button>
+            <button className="">Visit Ticket</button>
             <button className="">About</button>
+            <button className="">Log In</button>
           </div>
         </div>
       </div>
